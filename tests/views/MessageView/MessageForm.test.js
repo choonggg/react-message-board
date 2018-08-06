4605f7b56395cfc08b06519450b2a64ac3ea423e
Chong Hui <iamchoonggg@gmail.com>
2018-08-06 20:02:04 +0800

    change render to mount for slight speed bump

--- tests/views/MessageView/MessageForm.test.js
+++ tests/views/MessageView/MessageForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from 'enzyme'
+import { mount } from 'enzyme'
 
 import MessageStore from '@/stores/MessageStore'
 import MessageForm from '@/views/MessageView/MessageForm'
@@ -7,7 +7,7 @@ import MessageForm from '@/views/MessageView/MessageForm'
 describe('<MessageForm />', () => {
   describe('renderButton() submitting is false', () => {
     it('renders "Post message"', () => {
-      const wrapper = render(<MessageForm messageStore={MessageStore} />)
+      const wrapper = mount(<MessageForm messageStore={MessageStore} />)
       const submitHtml = wrapper.find('button#MessageForm-submit').text()
 
       expect(submitHtml).toEqual("Post message")
@@ -17,7 +17,7 @@ describe('<MessageForm />', () => {
   describe('renderButton() submitting is true', () => {
     it('renders ProgressBar', () => {
       MessageStore.submitting = true
-      const wrapper = render(<MessageForm messageStore={MessageStore} />)
+      const wrapper = mount(<MessageForm messageStore={MessageStore} />)
       const submitHtml = wrapper.find('button#MessageForm-submit').html()
 
       expect(submitHtml).toContain("ProgressBar")
